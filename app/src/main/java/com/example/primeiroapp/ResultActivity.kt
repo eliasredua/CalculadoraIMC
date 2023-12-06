c44eb2bfdc4dce6774dbc6c1f4b7d64efe0237dd
eliasredua <eliasredua@gmail.com>
2023-12-06 07:05:28 -0300

    add

---
+++ app/src/main/java/com/example/primeiroapp/ResultActivity.kt
@@ -0,0 +1,41 @@
+package com.example.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvResult = findViewById<TextView>(R.id.textview_imc)
+        val tvClassificacao = findViewById<TextView>(R.id.textview_classificacao)
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+      val classificacao = if(result < 18.5f){
+            "MAGREZA"
+        } else if(result in 18.5f..24.9f){
+            "NORMAL"
+        } else if(result in 25f..29.9f){
+            "SOBREPESO"
+        } else if(result in 30f..39.9f){
+            "OBESO"
+        } else{
+            "OBESIDADE GRAVE"
+        }
+
+        tvClassificacao.text = getString(R.string.message_classificacao, classificacao)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
